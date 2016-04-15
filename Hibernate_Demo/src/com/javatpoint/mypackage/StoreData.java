93f93f98173fe33538e98764cbb114694ebb142e
lakzso <lakzsolt@gmail.com>
2016-04-15 17:06:57 +0200

    mai

--- Hibernate_Demo/src/com/javatpoint/mypackage/StoreData.java
+++ Hibernate_Demo/src/com/javatpoint/mypackage/StoreData.java
@@ -1,10 +1,16 @@
 package com.javatpoint.mypackage;  
   
+import java.util.List;
+import java.util.ListIterator;
+
 import org.hibernate.Query;
 import org.hibernate.Session;  
 import org.hibernate.SessionFactory;  
 import org.hibernate.Transaction;  
-import org.hibernate.cfg.Configuration;  
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+
   
 public class StoreData {  
 public static void main(String[] args) {  
@@ -23,17 +29,38 @@ public static void main(String[] args) {
     Transaction t=session.beginTransaction();  
           
     Employee e1=new Employee();  
-    e1.setId(115);  
-    e1.setFirstName("sonoo");  
-    e1.setLastName("jaiswal");  
+    e1.setId(117);  
+    e1.setFirstName("Zsolt2");  
+    e1.setLastName("Lakatos2");  
+    //session.persist(e1);
+    Query q=session.createQuery("from Employee where LastName=:Name");
+    q.setParameter("Name", "Lakatos");
+    List l=q.list();
+    ListIterator iteral=l.listIterator();
+    
+    while (iteral.hasNext()){
+    	
+    	Employee e=(Employee)iteral.next();
+    	System.out.print(e.getFirstName()+e.getLastName());
+    }
+   
+    Employee emp=(Employee) session.get(Employee.class, 116);
+    System.out.print(emp.getFirstName()+emp.getLastName());
       
-    session.persist(e1);//persisting the object  
-      
+    List ems=session.createCriteria(Employee.class).add(Restrictions.like("firstName", "%s%")).list();
+    iteral=ems.listIterator();
+	    
+	while (iteral.hasNext()){
+	    	
+	    	Employee e=(Employee)iteral.next();
+	    	System.out.print(e.getFirstName()+e.getLastName());
+	    }
+    
+    
     t.commit();//transaction is committed  
 
     session.close();  
-      
-    System.out.println("successfully saved");  
+
       
 }  
 }  
